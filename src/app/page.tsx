58097de1e02007702096c47d67fa77964c8a0ebf
Victor101106 <victorgabriel101106+github@gmail.com>
2025-02-20 15:30:27 -0300

    feat: add github repository button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { IconButtonView } from "@/components/IconButton";
 import {
   TextEditorView,
   useTextEditorViewModel,
@@ -8,6 +9,7 @@ import {
   useWordCounterViewModel,
   WordCounterView,
 } from "@/components/WordCounter";
+import { RiGithubFill } from "@remixicon/react";
 
 export default function Home() {
   const textEditorViewModel = useTextEditorViewModel();
@@ -18,8 +20,16 @@ export default function Home() {
 
   return (
     <main className="flex flex-col h-dvh w-dvw overflow-hidden">
+      <div className="lg:fixed lg:bg-transparent bg-background w-full top-0 left-0 p-3.5 lg:p-5 flex flex-row-reverse justify-between">
+        <IconButtonView
+          icon={RiGithubFill}
+          onClick={() => {
+            window.open("https://github.com/Victor101106/ParvaNota", "_blank");
+          }}
+        />
+      </div>
       <TextEditorView {...textEditorViewModel} />
-      <div className="lg:fixed lg:bg-transparent bg-background w-full bottom-0 left-0 p-5 flex flex-row-reverse justify-between">
+      <div className="lg:fixed lg:bg-transparent bg-background w-full bottom-0 left-0 p-3.5 lg:p-5 flex flex-row-reverse justify-between">
         <WordCounterView {...wordCounterViewModel} />
       </div>
     </main>
--- src/components/TextEditor/TextEditorView.tsx
+++ src/components/TextEditor/TextEditorView.tsx
@@ -10,7 +10,7 @@ export type TextEditorViewProps = TextEditorViewModelPickedReturnType;
 export function TextEditorView(props: TextEditorViewProps) {
   return (
     <textarea
-      className="w-full h-full focus:outline-none overflow-y-scroll resize-none scroll:bg-red bg-background text-foreground px-[8%] lg:px-[25%] py-12 lg:py-32 text-justify text-[0.9375rem] leading-[1.40625rem] font-normal tracking-[0%] selection:bg-selection-background selection:text-selection-foreground"
+      className="w-full h-full focus:outline-none overflow-y-scroll resize-none scroll:bg-red bg-background text-foreground px-[8%] lg:px-[25%] lg:py-32 text-justify text-[0.9375rem] leading-[1.40625rem] font-normal tracking-[0%] selection:bg-selection-background selection:text-selection-foreground"
       placeholder={"Welcome to ParvaNota!\nEnter or paste your text here..."}
       onChange={props.onTextEditorChange}
       onSelect={props.onTextEditorSelect}
