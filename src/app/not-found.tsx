b90cbc736f8007e1806a7f105f79237572bab8d6
Victor101106 <victorgabriel101106+github@gmail.com>
2025-02-21 16:08:17 -0300

    fix(not-found): remove unnecessary client component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/");
-  });
+  redirect("/");
 }
